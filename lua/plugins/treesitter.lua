d0179f9cce46f4f7f1200ee1d5f3a5abb08f968a
Jaelse Ronald <jaelse.ronald@gmail.com>
2026-01-13 10:53:25 +0000

    new plugins git, fidgits and other stuff

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,16 @@
 return {
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    opts = {
-      auto_install=true,
-    }
-  }
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+        require("nvim-treesitter").setup({
+            install_dir = vim.fn.stdpath("data") .. "/site",
+            auto_install = true,
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = { "ruby" },
+            },
+            indent = { enable = true, disable = { "ruby" } },
+        })
+    -- installed all the language support by :TSInstall all
+    end,
+}
