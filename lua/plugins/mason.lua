d73f100b0e29eb1539bba41787ade98e662a4753
Jaelse Ronald <jaelse.ronald@gmail.com>
2026-01-19 11:31:39 +0000

    adding pylsp

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,48 +1,49 @@
 return {
-  {
-    "mason-org/mason.nvim",
-    opts = {},
-    config = function()
-      require("mason").setup({
-        registries = {
-          "github:mason-org/mason-registry",
-          "github:Crashdummyy/mason-registry",
-        },
-      })
-    end,
-  },
-  {
-    "mason-org/mason-lspconfig.nvim",
-    opts = {},
-    dependencies = {
-      { "mason-org/mason.nvim", opts = {} },
-      "neovim/nvim-lspconfig",
+    {
+        "mason-org/mason.nvim",
+        opts = {},
+        config = function()
+            require("mason").setup({
+                registries = {
+                    "github:mason-org/mason-registry",
+                    "github:Crashdummyy/mason-registry",
+                },
+            })
+        end,
     },
-    config = function()
-      require("mason-lspconfig").setup()
-    end,
-  },
-  {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    config = function()
-      require("mason-tool-installer").setup({
-        ensure_installed = {
-          "lua_ls",
-          "vimls",
-          "rust_analyzer",
-          "ts_ls",
-          "bashls",
-          "html",
-          "cssls",
-          "tailwindcss",
-          "svelte",
-          "prismals",
-          "pyright",
-          "eslint",
-          "bashls",
-          "gopls",
+    {
+        "mason-org/mason-lspconfig.nvim",
+        opts = {},
+        dependencies = {
+            { "mason-org/mason.nvim", opts = {} },
+            "neovim/nvim-lspconfig",
         },
-      })
-    end,
-  },
+        config = function()
+            require("mason-lspconfig").setup()
+        end,
+    },
+    {
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+        config = function()
+            require("mason-tool-installer").setup({
+                ensure_installed = {
+                    "lua_ls",
+                    "vimls",
+                    "rust_analyzer",
+                    "ts_ls",
+                    "bashls",
+                    "html",
+                    "cssls",
+                    "tailwindcss",
+                    "svelte",
+                    "prismals",
+                    "pyright",
+                    "eslint",
+                    "bashls",
+                    "gopls",
+                    "pylsp",
+                },
+            })
+        end,
+    },
 }
