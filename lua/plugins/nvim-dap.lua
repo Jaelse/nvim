6a6a5b069bd393418caa2994583da5ddcd73ddc6
Jaelse Ronald <jaelse.ronald@gmail.com>
2026-02-02 09:02:56 +0000

    making debug key to q

--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -1,3 +1,29 @@
 return {
     "mfussenegger/nvim-dap",
+    config = function()
+        vim.keymap.set("n", "<leader>ql", "<cmd>lua require'dap'.step_into()<CR>", { desc = "Debugger step into" })
+        vim.keymap.set("n", "<leader>qj", "<cmd>lua require'dap'.step_over()<CR>", { desc = "Debugger step over" })
+        vim.keymap.set("n", "<leader>qk", "<cmd>lua require'dap'.step_out()<CR>", { desc = "Debugger step out" })
+        vim.keymap.set("n", "<leader>qc", "<cmd>lua require'dap'.continue()<CR>", { desc = "Debugger continue" })
+        vim.keymap.set(
+            "n",
+            "<leader>qb",
+            "<cmd>lua require'dap'.togger_breakpoint()<CR>",
+            { desc = "Debugger toggle breakpoint" }
+        )
+        vim.keymap.set(
+            "n",
+            "<leader>qd",
+            "<cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>",
+            { desc = "Debugger set conditional breakpoint" }
+        )
+        vim.keymap.set("n", "<leader>qe", "<cmd>lua require'dap'.terminate()<CR>", { desc = "Debugger reset" })
+        vim.keymap.set("n", "<leader>qr", "<cmd>lua require'dap'.run_last()<CR>", { desc = "Debugger run last" })
+        vim.keymap.set(
+            "n",
+            "<leader>qt",
+            "<cmd>lua vim.cmd('RustLsp testables')<CR>",
+            { desc = "Debugger test tables" }
+        )
+    end,
 }
