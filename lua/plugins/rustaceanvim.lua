8e186b038d1bc5eadeb9ef743c66dd8683c74f75
Jaelse Ronald <jaelse.ronald@gmail.com>
2026-02-02 11:28:33 +0000

    rust analyzer all features true

--- lua/plugins/rustaceanvim.lua
+++ lua/plugins/rustaceanvim.lua
@@ -2,4 +2,9 @@ return {
     "mrcjkb/rustaceanvim",
     version = "^7",
     lazy = false,
-   }
+    ["rust-analyzer"] = {
+        cargo = {
+            allFeatures = true,
+        },
+    },
+}
