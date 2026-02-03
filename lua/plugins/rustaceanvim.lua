6295246ca916e0541636787e70decff2c53e168a
Jaelse Ronald <jaelse.ronald@gmail.com>
2026-02-03 02:45:46 +0000

    rust keymaps

--- lua/plugins/rustaceanvim.lua
+++ lua/plugins/rustaceanvim.lua
@@ -7,4 +7,14 @@ return {
             allFeatures = true,
         },
     },
+    config = function()
+        local bufnr = vim.api.nvim_get_current_buf()
+        vim.keymap.set("n", "<leader>gra", function()
+            vim.cmd.RustLsp("codeAction")
+        end, { silent = true, buffer = bufnr })
+
+        vim.keymap.set("n", "K", function()
+            vim.cmd.RustLsp({ "hover", "actions" })
+        end, { silent = true, buffer = bufnr })
+    end,
 }
