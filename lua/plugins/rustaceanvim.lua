83ad74880f1623d89156d2d8718534e75830a82b
Jaelse Ronald <jaelse.ronald@gmail.com>
2026-05-20 04:37:47 +0000

    rust file format on save

--- lua/plugins/rustaceanvim.lua
+++ lua/plugins/rustaceanvim.lua
@@ -14,13 +14,19 @@ return {
 
         vim.g.rustaceanvim = {
             server = {
-                on_attach = function()
+                on_attach = function(_, bufnr)
                     vim.keymap.set(
                         "n",
                         "gI",
                         vim.lsp.buf.implementation,
                         { buffer = bufnr, desc = "Go to implementation" }
                     )
+                    vim.api.nvim_create_autocmd("BufWritePre", {
+                        buffer = bufnr,
+                        callback = function()
+                            vim.lsp.buf.format({ bufnr = bufnr, async = false })
+                        end,
+                    })
                 end,
                 settings = {
                     ["rust-analyzer"] = {
