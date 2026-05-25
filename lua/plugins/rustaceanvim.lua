a12cf69b11f00a549f582beaf70bfa0a251bd55d
Jaelse Ronald <jaelse.ronald@gmail.com>
2026-05-25 09:15:41 +0000

    save rust files after 2 sec

--- lua/plugins/rustaceanvim.lua
+++ lua/plugins/rustaceanvim.lua
@@ -27,6 +27,20 @@ return {
                             vim.lsp.buf.format({ bufnr = bufnr, async = false })
                         end,
                     })
+                    local timer = vim.uv.new_timer()
+                    vim.api.nvim_create_autocmd({ "TextChanged", "InsertLeave" }, {
+                        buffer = bufnr,
+                        callback = function()
+                            timer:stop()
+                            timer:start(2000, 0, vim.schedule_wrap(function()
+                                if vim.api.nvim_buf_is_valid(bufnr) and vim.bo[bufnr].modified then
+                                    vim.api.nvim_buf_call(bufnr, function()
+                                        vim.cmd("silent! write")
+                                    end)
+                                end
+                            end))
+                        end,
+                    })
                 end,
                 settings = {
                     ["rust-analyzer"] = {
