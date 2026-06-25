3f6c169a1bd7642a4f7791cca0392ee54a93fe3b
Jaelse Ronald <jaelse.ronald@gmail.com>
2026-06-25 11:06:59 +0000

    rolling back

--- lua/plugins/nvim-ts-context-commentstring.lua
+++ lua/plugins/nvim-ts-context-commentstring.lua
@@ -1,11 +1,3 @@
 return {
     "JoosepAlviste/nvim-ts-context-commentstring",
-    require("Comment").setup({
-        pre_hook = function(ctx)
-            local loaded, ts_context = pcall(require, "ts_context_commentstring.integrations.comment_nvim")
-            if loaded and ts_context then
-                return ts_context.create_pre_hook()(ctx)
-            end
-        end,
-    }),
 }
